6cc30473154f1b5294c889679e6c22ea3fffa9c2
Evuns <m_ateva@abv.bg>
2019-09-01 22:10:36 +0300

    1.PermutationWithoutRepetition.1

--- src/combinatorial/lab/PermutWithoutRepetitions.java
+++ src/combinatorial/lab/PermutWithoutRepetitions.java
@@ -3,7 +3,6 @@ package combinatorial.lab;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class PermutWithoutRepetitions {
     public static void main(String[] args) throws IOException {
