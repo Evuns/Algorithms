232b22b9067626907741d4da9445664feac89f26
Evuns <m_ateva@abv.bg>
2019-09-02 23:54:10 +0300

    3.InversionCount

---
+++ src/sortingAndSearchingAlgorithms/InversionCount.java
@@ -0,0 +1,29 @@
+package sortingAndSearchingAlgorithms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class InversionCount {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] input = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        insertionSort(0, input, 0);
+    }
+
+    private static void insertionSort(int index, int[] array, int counter) {
+        if (index == array.length) {
+            System.out.println(counter);
+        } else {
+            int num = array[index];
+            for (int i = index; i < array.length; i++) {
+                if (array[i] < num) {
+                    counter++;
+                }
+            }
+            insertionSort(index + 1, array, counter);
+        }
+    }
+}
